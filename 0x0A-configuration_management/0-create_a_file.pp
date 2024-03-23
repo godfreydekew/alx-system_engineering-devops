64f071e06280a5c2dd20cad89c3817da8158abf1
godfreydekew <dekewgodfrey@gmail.com>
2024-03-23 02:29:45 -0700

    Task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-
-file { 'school':
-  path    => '/tmp/school',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0744',
+file { '/tmp/school':
+  ensure => present,
+  mode   => '0744',
+  owner  => 'www-data',
+  group  => 'www-data',
   content => 'I love Puppet',
 }
