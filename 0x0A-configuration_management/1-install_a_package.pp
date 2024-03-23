922bf39adb9e015acf19c6e0f228186edaf001b2
godfreydekew <dekewgodfrey@gmail.com>
2024-03-23 03:19:55 -0700

    Fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
 #install flask from pip3
 
 package { 'flask':
-  ensure          => 'installed',
+  ensure          => '2.1.0',
   provider        => 'pip3',
-  install_options => [{'version' => '2.1.0'}]
+}
+
+package { 'werkzeug':
+  ensure	=> '2.1.1',
+  provider	=> 'pip3',
 }
