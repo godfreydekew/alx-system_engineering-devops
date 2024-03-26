3a5fc1edc9930f7bf076614f71d51b2d8de2efdd
godfreydekew <dekewgodfrey@gmail.com>
2024-03-26 09:47:28 -0700

    final update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #using pappet to male changes to our configuration file
 
-file { 'etc/ssh/ssh_cofig':
+file { '/etc/ssh/ssh_cofig':
 	ensure => present,
 content =>"
 	#ssh client configuration
-	host*
-	IdentityFile ~/.ssh/school
-	PasswordAuthentication no
+	Host*
+	  IdentityFile ~/.ssh/school
+	  PasswordAuthentication no
 	",
 }
