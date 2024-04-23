92cbf9df3415ea51d7a4e068777fec38e0b81073
godfreydekew <dekewgodfrey@gmail.com>
2024-04-23 00:58:45 -0700

    Update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     usr = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-    with open("file.csv", "w", encoding="utf-8") as f:
+    with open("{}.csv".format(usr.get("id")), "w", encoding="utf-8") as f:
         for todo in todos:
             f.write('"{}","{}","{}","{}"\n'.format(todo.get("userId"), usr.get(
                 "username"), todo.get("completed"), todo.get("title")))
