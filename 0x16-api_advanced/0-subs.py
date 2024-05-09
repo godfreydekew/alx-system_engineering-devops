4930db589bfdb1b1fcc707019b5f01eeadf27bf2
godfreydekew <dekewgodfrey@gmail.com>
2024-05-09 07:33:45 -0700

    Update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,10 +9,10 @@ def number_of_subscribers(subreddit):
     Args:
         subreddit (str): The name of the subreddit.
     """
-
-    url = f"https://api.reddit.com/r/{subreddit}/about.json"
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {"User-Agent": "My Reddit Subscriber Counter v1.0"}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    print(response.status_code)
     if response.status_code == 200:
         data = response.json()
         subscribers = data["data"]["subscribers"]
